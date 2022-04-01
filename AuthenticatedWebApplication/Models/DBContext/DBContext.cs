556938b064de7ccc4da7a3136a156958b35dcc58
ash-0910 <A00244471@mycambrian.ca>
2022-04-01 00:59:10 -0400

    Add project files.

---
+++ AuthenticatedWebApplication/Models/DBContext/DBContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthenticatedWebApplication.Models.DBContext
+{
+    public class DBContext: IdentityDbContext
+    {
+        private readonly DbContextOptions _options;
+
+        public DBContext(DbContextOptions options) : base(options)
+        {
+            _options = options;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
